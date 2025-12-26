896093cdebab68c8521ba8889d79d06ac76806b5
b0om1987 <105589045+b0om1987@users.noreply.github.com>
2025-12-26 22:56:22 +0500

    Initial commit
    First files requred for running this webapp

---
+++ main.py
@@ -0,0 +1,16 @@
+from telethon import TelegramClient, events
+import os
+
+client = TelegramClient(
+    'Edge Sesh', os.getenv('API_ID'), os.getenv('API_HASH'),
+    system_version = os.getenv('SYSTEM_VERSION'),
+    device_model = os.getenv('DEVICE_MODEL'),
+    app_version = os.getenv('APP_VERSION')).start(bot_token=os.getenv('TOKEN'))
+
+@client.on(events.NewMessage)
+async def my_event_handler(event):
+    if 'hello' in event.raw_text:
+        await event.reply('hi!')
+
+client.start()
+client.run_until_disconnected()
