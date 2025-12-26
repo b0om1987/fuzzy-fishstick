c323c6f3e19efa4df4e52f62c0a9545bfbfb6fe2
b0om1987 <105589045+b0om1987@users.noreply.github.com>
2025-12-27 02:22:46 +0500

    Update main.py
    For real this time

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ client = TelegramClient(
     system_version = os.getenv('SYSTEM_VERSION'),
     device_model = os.getenv('DEVICE_MODEL'),
     app_version = os.getenv('APP_VERSION'),
-    local_addr = f'0.0.0.0:{os.getenv('PORT')}').start(bot_token=os.getenv('TOKEN'))
+    local_addr = '0.0.0.0:19133').start(bot_token=os.getenv('TOKEN'))
 
 @client.on(events.NewMessage)
 async def my_event_handler(event):
@@ -15,3 +15,4 @@ async def my_event_handler(event):
 
 client.start()
 client.run_until_disconnected()
+
