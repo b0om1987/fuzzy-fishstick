9e43875c52951eb71214d1951a523ec87b09bc19
b0om1987 <105589045+b0om1987@users.noreply.github.com>
2025-12-27 02:17:34 +0500

    Update main.py
    Final IP fix for today

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ client = TelegramClient(
     system_version = os.getenv('SYSTEM_VERSION'),
     device_model = os.getenv('DEVICE_MODEL'),
     app_version = os.getenv('APP_VERSION'),
-    local_addr = [f'0.0.0.0:{os.getenv('PORT')}']).start(bot_token=os.getenv('TOKEN'))
+    local_addr = f'0.0.0.0:{os.getenv('PORT')}').start(bot_token=os.getenv('TOKEN'))
 
 @client.on(events.NewMessage)
 async def my_event_handler(event):
