50fffbf3a4fde1c4cc4936e9a73702ce299993fb
b0om1987 <105589045+b0om1987@users.noreply.github.com>
2025-12-27 12:39:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,26 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
   
-"""
 client = TelegramClient(
     'Edge Sesh', os.getenv('API_ID'), os.getenv('API_HASH'),
     system_version = os.getenv('SYSTEM_VERSION'),
     device_model = os.getenv('DEVICE_MODEL'),
-    app_version = os.getenv('APP_VERSION'),
-    local_addr = ('0.0.0.0', os.getenv('PORT'))).start(bot_token=os.getenv('TOKEN'))
+    app_version = os.getenv('APP_VERSION')).start(bot_token=os.getenv('TOKEN'))
 
 @client.on(events.NewMessage)
 async def my_event_handler(event):
     if 'hello' in event.raw_text:
         await event.reply('hi!')
-"""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=False)
-#client.start()
-#client.run_until_disconnected()
+client.start()
+client.run_until_disconnected()
+
 
 
