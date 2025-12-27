e5670645e5caa67624a9f9ba2f6293e2f88a064a
b0om1987 <105589045+b0om1987@users.noreply.github.com>
2025-12-27 11:46:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from telethon import TelegramClient, events
+from flask import Flask
 import os
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
 
 client = TelegramClient(
     'Edge Sesh', os.getenv('API_ID'), os.getenv('API_HASH'),
@@ -13,6 +19,7 @@ async def my_event_handler(event):
     if 'hello' in event.raw_text:
         await event.reply('hi!')
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=False)
 client.start()
 client.run_until_disconnected()
-
