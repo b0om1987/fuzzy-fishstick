7dffd5b694d3306a5df4609786b3f60b15df955f
b0om1987 <105589045+b0om1987@users.noreply.github.com>
2026-02-28 19:14:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -200,15 +200,16 @@ async def _mainline(event):
 
     if 'порно' in event.raw_text:
         keyboard = [
-            Button.inline("First option", b"1"),
-            Button.inline("Second option", b"2"),
-            Button.inline("Third option", b"3")
+            [Button.inline("First option", b"1")],
+            [Button.inline("Second option", b"2")],
+            [Button.inline("Third option", b"3")]
         ]
         await event.respond(f'весело задорно хули я ещё могу сказать {choice(mat)}', buttons=keyboard)
             
 
 client.start()
 client.run_until_disconnected()
+
 
 
 
