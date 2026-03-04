e5cec978c5242c16e32591990abe59752611d97e
b0om1987 <105589045+b0om1987@users.noreply.github.com>
2026-03-04 20:36:58 +0500

    Update main.py
    Hopefully fixed logic

--- main.py
+++ main.py
@@ -300,22 +300,22 @@ async def _mainline(event):
 @client.on(events.CallbackQuery())
 async def callback(event):
     
-    if str(event.sender_id).encode() == str(event.data[2:]):
+    if str(event.sender_id) == str(event.data)[4:-1]:
         account = userDatabase.find_one({'userId': str(event.sender_id)})
         
-        if event.data[:1] == b'11':
+        if str(event.data[2:4]) == '11':
             userDatabase.update_one(account, {'$set': {"loveIntrest": 11}})
             await event.answer('Вы успешно выбрали Дока!')
-        if event.data[:1] == b'12':
+        if str(event.data[2:4]) == '12':
             userDatabase.update_one(account, {'$set': {"loveIntrest": 12}})
             await event.answer('вы выбрали раста')
-        if event.data[:1] == b'13':
+        if str(event.data[2:4]) == '13':
             userDatabase.update_one(account, {'$set': {"loveIntrest": 13}})
             await event.answer('Вы успешно выбрали Зирована!')
-        if event.data[:1] == b'14':
+        if str(event.data[2:4]) == '14':
             userDatabase.update_one(account, {'$set': {"loveIntrest": 14}})
             await event.answer('Вы успешно выбрали Теслу!')
-        if event.data[:1] == 'b15':
+        if str(event.data[2:4]) == '15':
             userDatabase.update_one(account, {'$set': {"loveIntrest": 15}})
             await event.answer('Вы успешно выбрали Армстронга!')
     else:
@@ -323,6 +323,7 @@ async def callback(event):
 
 client.start()
 client.run_until_disconnected()
+
 
 
 
